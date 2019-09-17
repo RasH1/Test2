d35d03a18d020272ca66c80a706fc746d67def8a
RasH1 <rsadzo@gmail.com>
2019-09-17 09:51:26 +0200

    Nye filer
    Filer i reposity

---
+++ Exercise.js
@@ -0,0 +1,18 @@
+function insertText(){
+	document.getElementById("newContent").innerHTML = "Some new text: <br> Hello <br>";
+}
+
+function calculate (){
+	var t = widthBox.value;
+	// read from the textBoxs
+	var w = document.getElementById("widthBox").value;
+	var l = document.getElementById("lenghtBox").value;
+	var h = document.getElementById("heightBox").value;
+	//convert to Float
+	w=parseFloat(w);
+	h=parseFloat(h);
+	l=parseFloat(l);
+	var result =w*l*h;
+	//show result in div
+	document.getElementById("result").innerHTML = result;
+}
